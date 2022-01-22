5bd5b74634b0670ba4ddbc59ea5796e25ca71a22
andr3h3nriqu3s11 <andr3h3nriqu3s11@users.noreply.github.com>
2022-01-22 14:21:16 +0000

    deploy: 73133f78f4ee005c4ee7dab63ed2a5f4d3d6fc59

---
+++ _nuxt/static/1642861275/algorithms/DivideAndConquer/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/algorithms/DivideAndConquer", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1642861275/algorithms/Knapsack/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/algorithms/Knapsack", {data:[{}],fetch:{},mutations:void 0});
